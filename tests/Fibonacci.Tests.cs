129158a384198a4fa53caf9ab1a7fae0acb64eba
Bru No <brunocorona.alcantar@gmail.com>
2015-09-25 12:44:25 -0500

    Fix Fibonacci

--- tests/Fibonacci.Tests.cs
+++ tests/Fibonacci.Tests.cs
@@ -5,12 +5,16 @@ namespace CSharp.Training.Tests
     public class FibonacciTests
     {
         Fibonacci fibo = new Fibonacci();
-        [Test]
-        public void AddTest()
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(7, 13)]
+        [TestCase(17, 1597)]
+        [TestCase(43, 433494437)]
+        [Timeout(10000)]
+        public void AddTest(int input, int output)
         {
-            Assert.That(fibo.Compute(2).Equals(" 1 2"));
-            Assert.That(fibo.Compute(5).Equals(" 1 2 3 5 8"));
-            Assert.That(fibo.Compute(8).Equals(" 1 2 3 5 8 13 21 34"));
+            Assert.That(fibo.Compute(input).Equals(output));
         }
     }
 }
